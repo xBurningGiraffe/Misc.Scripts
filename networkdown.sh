26910737edf5e31aa4be92258f383e2f4fdd2b95
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2022-10-05 11:36:03 -0500

    Add files via upload

---
+++ networkdown.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# OpenDNS
+TARGET_IP1=208.67.220.220
+TARGET_IP2=208.67.222.222
+
+# Ping IPs
+count1=$(ping -c 5 $TARGET_IP1 | grep from* | wc -l)
+count2=$(ping -c 5 $TARGET_IP2 | grep from* | wc -l)
+
+if [ $count1 -eq 0 ] ; then
+    /sbin/shutdown -r 0
+elif
+    [ $count2 -eq 0 ] ; then
+    /sbin/shutdown -r 0
+else
+# Network is up
+    :
+fi
