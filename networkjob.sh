951f4922d3056368e736d5cb2bea8f127c8f8b47
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2022-10-07 16:09:57 -0500

    Update networkjob.sh

--- networkjob.sh
+++ networkjob.sh
@@ -4,7 +4,7 @@ mkdir /opt/rader
 wget https://raw.githubusercontent.com/xBurningGiraffe/Misc.Scripts/main/networkcheck.sh -P /opt/rader/networkcheck.sh
 chmod +x /opt/rader/networkcheck.sh
 crontab -l > mycron
-echo "0 */2 * * *" /opt/rader/networkdown.sh >> mycron
+echo "0 */2 * * * /opt/rader/networkdown.sh" >> mycron
 crontab mycron
 rm -f mycron
 rm -f networkjob.sh
