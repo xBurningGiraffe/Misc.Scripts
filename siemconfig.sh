439bdb8fa8f31915549a61b98800d74b0f2b5e5a
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2022-11-08 12:58:30 -0600

    Update siemconfig.sh

--- siemconfig.sh
+++ siemconfig.sh
@@ -3,14 +3,7 @@
 sudo yum -y install perch_siem tcpdump nmap wget unzip # Installs
 
 #LT install
-mkdir -p /tmp/lt_install
-cd /tmp/lt_install
-wget -O lt.zip http://labtech.radersolutions.com/labtech/transfer/installers/LTechAgent_x86_64_loc_412.zip
-unzip lt.zip
-cd LTechAgent
-chmod a+x install.sh
-./install.sh
-rm -rf /tmp/lt11_install
+sudo curl https://labtech.radersolutions.com/lt.sh | sh
 
 # networkcheck install
 wget https://raw.githubusercontent.com/xBurningGiraffe/Misc.Scripts/main/networkjob.sh
