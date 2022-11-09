bb2cb8757e63a06379d5996375e153ead9f62e79
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2022-11-09 15:19:42 -0600

    Update siemconfig.sh

--- siemconfig.sh
+++ siemconfig.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo yum -y install perch_siem tcpdump nmap unzip # Installs
-
-#LT install
-sudo curl https://labtech.radersolutions.com/lt.sh | sh
+sudo yum -y install wget perch_siem tcpdump nmap unzip # Installs
 
 # networkcheck install
 wget https://raw.githubusercontent.com/xBurningGiraffe/Misc.Scripts/main/networkjob.sh
@@ -11,4 +8,5 @@ chmod +x networkjob.sh
 ./networkjob.sh
 rm -f siemconfig.sh
 
+echo "Proceed with installing Labtech"
 
