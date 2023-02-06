d6ca4a73cad89b1b10bec667123723120046965a
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-02-06 12:07:25 -0600

    Update restarthyperv.ps1

--- restarthyperv.ps1
+++ restarthyperv.ps1
@@ -4,8 +4,7 @@ foreach ($HyperV in $HyperVs) {
 $Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-Command `"Get-Service -Name '$HyperV' | Where-Object { $_.Status -ne 'Running' } | ForEach-Object { $_.Start() }`""
 $Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1)
 $Settings = New-ScheduledTaskSettingsSet
-$Principal = New-ScheduledTaskPrincipal -UserID "$env:UserDomain\$env:Username"
+# $Principal = New-ScheduledTaskPrincipal -UserID "$env:UserDomain\$env:Username"
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-
 Register-ScheduledTask -TaskName 'RestartService' -InputObject $Task
 }
