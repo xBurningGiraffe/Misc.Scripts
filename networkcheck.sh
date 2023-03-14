27a50996565e4abd9616c4963224db7397db4733
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-03-14 10:48:38 -0500

    Update networkcheck.sh
    Made network check more specific

--- networkcheck.sh
+++ networkcheck.sh
@@ -3,17 +3,30 @@
 # OpenDNS
 TARGET_IP1=208.67.220.220
 TARGET_IP2=208.67.222.222
+# Google DNS
+TARGET_IP3=8.8.8.8
+TARGET_IP4=8.8.4.4
 
-# Ping IPs
-count1=$(ping -c 5 $TARGET_IP1 | grep from* | wc -l)
-count2=$(ping -c 5 $TARGET_IP2 | grep from* | wc -l)
-
-if [ $count1 -eq 0 ] ; then
-    /sbin/reboot
-elif
-    [ $count2 -eq 0 ] ; then
-    /sbin/reboot
-else
-# Network is up
-    :
+# Check network connectivity
+ping -c 5 $TARGET_IP1 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  exit 0
 fi
+
+ping -c 5 $TARGET_IP2 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  exit 0
+fi
+
+ping -c 5 $TARGET_IP3 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  exit 0
+fi
+
+ping -c 5 $TARGET_IP4 > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  exit 0
+fi
+
+# Reboots if no IPs are responding
+/sbin/reboot
