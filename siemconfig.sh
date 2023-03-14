80c024a2cebd76bfd63cc9830faa06ae70986bcb
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-03-14 13:18:57 -0500

    Update siemconfig.sh
    Added functionality

--- siemconfig.sh
+++ siemconfig.sh
@@ -2,11 +2,29 @@
 
 sudo yum -y install wget perch_siem tcpdump nmap unzip # Installs
 
+# RADER Directory
+dir_path="/opt/rader"
+
+# Check for RADER Directory
+if [ -d "$dir_path" ]; then
+  echo "Directory already exists: $dir_path"
+else
+  sudo mkdir -p $dir_path
+  
+  sudo chmod 755 $dir_path
+  
+  sudo chown -R $USER:$USER $dir_path
+fi
+
 # networkcheck install
-wget https://raw.githubusercontent.com/xBurningGiraffe/Misc.Scripts/main/networkjob.sh
-chmod +x networkjob.sh
-./networkjob.sh
-rm -f siemconfig.sh
+sudo wget -O $dir_path/networkcheck.sh https://raw.githubusercontent.com/xBurningGiraffe/Misc.Scripts/main/networkcheck.sh
+sudo chmod +x $dir_path/networkcheck.sh
+
+# Cronjob for networkcheck.sh
+crontab -l > mycron
+echo "0 */2 * * * $dir_path/networkcheck.sh" >> mycron
+crontab mycron
+rm mycron
 
 echo "Proceed with installing Labtech"
 
