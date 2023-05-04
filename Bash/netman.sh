beb30c942807bb786a5ba07ca45e529acc1a5842
xBurningGiraffe <86382301+xBurningGiraffe@users.noreply.github.com>
2023-05-04 09:34:57 -0500

    Update netman.sh

--- Bash/netman.sh
+++ Bash/netman.sh
@@ -5,10 +5,10 @@ sudo bash -c 'cat << EOF > add_dns_none.sh
 nm_conf="/etc/NetworkManager/NetworkManager.conf"
 
 # Add the dns=none line to the [main] section
-sed -i "/^\[main\]$/a dns=none" $nm_conf
+sudo sed -i "/^\[main\]$/a dns=none" $nm_conf
 
 # Restart the NetworkManager service
-systemctl restart NetworkManager.service
+sudo systemctl restart NetworkManager.service
 
 # Print a message to indicate the script has finished
 echo "Added 'dns=none' to the [main] section in $nm_conf and restarted the NetworkManager service."
